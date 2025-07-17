7b4703f910652ee3c1790723678a0277cd8f6bb6
kvoon <kvoon@duck.com>
2025-07-18 01:22:37 +0800

    chore: remove playground

--- playground/README.md
+++
@@ -1,9 +0,0 @@
-# Playground
-
-```sh
-pnpm install
-```
-
-```sh
-pnpm run dev
-```
--- playground/src/mocks/handlers.js
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse } from 'msw'
-
-export const handlers = [
-  http.post('/message', async ({ request }) => {
-    const body = await request.json()
-    return HttpResponse.json(body)
-  }),
-]
