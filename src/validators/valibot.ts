7deaff1ca24e8ec63fcc56f4c18ae434e2ff7297
kvoon <kvoon@duck.com>
2025-07-18 14:23:36 +0800

    feat: configurable valibot rule parse

--- src/validators/valibot.ts
+++ src/validators/valibot.ts
@@ -1,8 +1,10 @@
-import type { ObjectSchema } from 'valibot'
+import type { BaseIssue, BaseSchema, InferIssue, ObjectSchema } from 'valibot'
 import type { ArcoRules } from '../types'
 import * as v from 'valibot'
 
-export function genValibotRules(schema: ObjectSchema<any, any>): ArcoRules {
+type Config = v.Config<InferIssue<BaseSchema<unknown, unknown, BaseIssue<unknown>>>>
+
+export function genValibotRules(schema: ObjectSchema<any, any>, config?: Config): ArcoRules {
   const rules: ArcoRules = {}
 
   const entries = schema.entries
@@ -10,7 +12,7 @@ export function genValibotRules(schema: ObjectSchema<any, any>): ArcoRules {
   for (const key in entries) {
     rules[key] = {
       validator(value, callback) {
-        const output = v.safeParse(entries[key], value)
+        const output = v.safeParse(entries[key], value, config)
         if (!output.success) {
           callback(output.issues[0].message)
         }
